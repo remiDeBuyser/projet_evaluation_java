a4227a12dce988e7a27c6a4558294bab17901cf8
remiDeBuyser <debuyser.remi@outlook.fr>
2022-01-14 11:34:42 +0100

    feat: distance Cas

--- src/projet_evaluation_java/Cas.java
+++ src/projet_evaluation_java/Cas.java
@@ -33,4 +33,34 @@ public class Cas {
 	public int getId() {
 		return id;
 	}
+	
+	public float getSimilarity(Cas Pj) {
+		return 1 - distance(Pj);
+	}
+	
+	public float distance(Cas Pj) {
+		int dist = 0;
+		int size = this.triplets.size();
+		
+		if ( size != Pj.getTriplets().size() ) return 1;
+		else {
+			boolean equal = true;
+			boolean notequal = true;
+			
+			for (int i = 0; i<size; i++) {
+				if (!this.triplets.get(i).equals(Pj.getTriplets().get(i))) equal = false;
+				if (this.triplets.get(i).equals(Pj.getTriplets().get(i))) notequal = false;
+			}
+			if (equal) return 0;
+			if (notequal) return 1;
+		}
+		
+		int somme = 0;
+		for (int i = 0; i<size; i++) {
+			somme += this.triplets.get(i).distance(Pj.getTriplets().get(i));
+		}
+		
+		return somme/(3*size);
+	}
+
 }
